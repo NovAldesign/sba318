bbb0611fba0eefa6d2649c3d566a33057123d9e7
NovAldesign <williams.lavon@gmail.com>
2026-01-29 08:00:11 -0500

    updated logging middleware to post data from intake form

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,22 +1,32 @@
 // 1. Logging Middleware 
 export function logReq(req, res, next) {
+    // (GET/POST)
     console.log(`${req.method} - ${req.url}`);
 
+    // shows data in the body if applicant submitted form
     if (req.body && Object.keys(req.body).length > 0) {
         console.log(`Req Data:`, req.body);
     }
 
-    next();
+    next(); // Goes to the next function in our stack
 }
 
-// 2. Custom Validation Middleware 
+// 2. Custom Validation Middleware: verifies if application is complete
 export function validateIntake(req, res, next) {
+
+    //  Checks to see if req.body isn't empty so the server doesn't crash
+    if (!req.body || Object.keys(req.body).length === 0) {
+        const error = new Error("No data received. Please fill out the form.");
+        error.status = 400;
+        return next(error);
+    }
+
     const { name, email, phone, industry, hopes, contribution } = req.body;
 
-    // Check for missing professional fields
+    // Check for missing fields on application
     if (!name || !email || !phone || !industry || !hopes || !contribution) {
         const error = new Error("Membership requires a complete profile. Please fill out all fields.");
-        error.status = 400;
+        error.status = 400; // Client error
         return next(error);
     }
 
@@ -28,16 +38,17 @@ export function validateIntake(req, res, next) {
         return next(error);
     }
 
-    next();
+    next(); // All checks passed. It takes you to the next router.
 }
 
 // 3. Global Error Handling Middleware 
 export function globalErr(err, req, res, next) {
+    // Log error to the console
     console.log(`Server Error: ${err.message}`);
-    
+
     // Return JSON 
-    res.status(err.status || 500).json({ 
-        success: false, 
-        error: err.message 
+    res.status(err.status || 500).json({
+        success: false,
+        error: err.message
     });
 }
